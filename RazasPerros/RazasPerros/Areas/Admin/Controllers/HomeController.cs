98b9571843f6c9e858e482e75523e5a46ced94d6
Hector Saul Maldonado Gomez <saul_99_27@hotmail.com>
2020-11-02 16:09:43 -0600

    Avance

--- RazasPerros/RazasPerros/Areas/Admin/Controllers/HomeController.cs
+++ RazasPerros/RazasPerros/Areas/Admin/Controllers/HomeController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using RazasPerros.Models;
+using RazasPerros.Repositories;
 
 namespace RazasPerros.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(string id)
         {
-            return View();
+            RazasRepository repos = new RazasRepository();
+            IndexViewModel vm = new IndexViewModel
+            {
+                Razas = id == null ? repos.GetRazas() : repos.GetRazasByLetraInicial(id),
+                LetrasIniciales = repos.GetLetrasIniciales()
+            };
+            return View(vm);
         }
         public IActionResult Agregar()
         {
