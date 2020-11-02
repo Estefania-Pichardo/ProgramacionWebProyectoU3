6f6c3e9d6783f39640a098db9f1c435b6b78e763
Estefania Pichardo <estefania.pichardo@r2devpros.com>
2020-11-02 01:39:01 -0600

    Proyecto Razas Perros

---
+++ RazasPerros/RazasPerros/Models/ViewModels/InfoPerroViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazasPerros.Models
+{
+	public class InfoPerroViewModel
+	{
+		public Razas Raza { get; set; }
+		public IEnumerable<RazaViewModel> OtrasRazas { get; set; }
+	}
+
+}
---
+++ RazasPerros/RazasPerros/Repositories/RazasRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RazasPerros.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace RazasPerros.Repositories
+{
+	public class RazasRepository
+	{
+		perrosContext context = new perrosContext();
+
+		public IEnumerable<RazaViewModel> GetRazas()
+		{
+			return context.Razas.OrderBy(x => x.Nombre)
+				.Select(x => new RazaViewModel
+				{
+					Id = x.Id,
+					Nombre = x.Nombre
+				});
+		}
+
+		public IEnumerable<RazaViewModel> GetRazasByLetraInicial(string letra)
+		{
+			return GetRazas().Where(x => x.Nombre.StartsWith(letra));
+		}
+
+
+		public IEnumerable<char> GetLetrasIniciales()
+		{
+			return context.Razas.OrderBy(x => x.Nombre)
+				.Select(x => x.Nombre.First()).Distinct();
+		}
+
+		public Razas GetRazaByNombre(string nombre)
+		{
+			nombre = nombre.Replace("-", " ");
+			return context.Razas
+				.Include(x => x.Estadisticasraza)
+				.Include(x => x.Caracteristicasfisicas)
+				.Include(x => x.IdPaisNavigation)
+				.FirstOrDefault(x => x.Nombre == nombre);
+		}
+
+		public IEnumerable<RazaViewModel> Get4RandomRazasExcept(string nombre)
+		{
+			nombre = nombre.Replace("-", " ");
+			Random r = new Random();
+			return context.Razas
+				.Where(x => x.Nombre != nombre)
+				.OrderBy(x => r.Next())
+				.Take(4)
+				.Select(x => new RazaViewModel { Id = x.Id, Nombre = x.Nombre });
+		}
+
+	}
+}
